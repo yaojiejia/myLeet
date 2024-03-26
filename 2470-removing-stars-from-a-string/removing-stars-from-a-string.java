9bbafc979aa65832f013652895636f0d9ac159e2
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-03-26 17:09:19 -0400

    Time: 79 ms (52.03%) | Memory: 46.6 MB (28.97%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<Integer> s1 = new Stack<Integer>(); // Correct stack to store indices of non-consecutive '*'
+        for (int i = 0; i < s.length(); i++) {
+            if ( s.charAt(i) != '*') {
+                s1.push(i);
+            }
+            else if( s.charAt(i) == '*'){
+                s1.pop();
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        while (!s1.isEmpty()) {
+            result.append(s.charAt(s1.pop()));
+        }
+        return result.reverse().toString();
+    }
+}
