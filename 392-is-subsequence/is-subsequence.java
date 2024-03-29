b4a7c2b820c6472d0bfc003f9470eac40fcb3989
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-03-29 00:14:52 -0400

    Time: 1 ms (90.83%) | Memory: 41.5 MB (24.05%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int position = -1; // Initialize position to -1
-        for(int i = 0; i < s.length(); i++) {
-            boolean found = false; // Flag to indicate if character is found
-            for(int j = position + 1; j < t.length(); j++) {
-                if(s.charAt(i) == t.charAt(j)) {
-                    found = true; // Set flag to true if character is found
-                    position = j; // Update position
-                    break; // Exit inner loop
+        int position = -1;
+        for(int i = 0; i < s.length(); i++){
+            boolean found = false;
+            for(int j = position+1; j < t.length(); j++){
+                if(s.charAt(i) == t.charAt(j)){
+                    found = true;
+                    position = j;
+                    break;
                 }
-            }
-            if(!found) return false; // If character not found, return false
+            
         }
-        return true;
+        if(!(found)) return false;
     }
+    return true;
+}
 }
