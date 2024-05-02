5902ad90876e411e044aebf36e2a5843164555f6
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-05-02 13:18:48 -0400

    Time: 52 ms (57.92%) | Memory: 15.6 MB (33.33%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,18 +1,8 @@
 class Solution(object):
     def countBits(self, n):
-        ans = []
-        for i in range(0, n+1):
-            ans.append(self.hammingWeight(i))
+        output = []
+        for i in range(n + 1):
+            value = bin(i).count("1")
+            output.append(value)
+        return output
         
-        return ans
-        
-        
-    def hammingWeight(self, n):
-        binary = bin(n)
-        st = str(binary)
-        count = 0
-        for i in range(0, len(st)):
-            if st[i] == '1':
-                count+=1
-        
-        return count
