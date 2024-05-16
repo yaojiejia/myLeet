1346e15e3f65d41e6e13ad76dcff96c1aa26dc91
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-05-16 15:27:19 -0700

    Time: 941 ms (44.05%) | Memory: 21.1 MB (79.76%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,8 +3,8 @@ class Solution(object):
         starting = sum(nums[:k])
         max_average = starting / float(k)
 
-        for i in range(k, len(nums)):
-            starting += nums[i] - nums[i - k]
+        for i in range(1, len(nums) - k + 1):
+            starting += nums[i + k - 1] - nums[i - 1]
             max_average = max(max_average, starting / float(k))
-
+        
         return max_average
