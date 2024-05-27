300a962010a11ef01de879c4fc2cf697c9fd1470
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-05-26 18:12:06 -0700

    Time: 12 ms (98.72%) | Memory: 11.8 MB (92.93%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -8,16 +8,16 @@ class Solution(object):
             if nums[m] == target:
                 return m
 
-            # Check if the left half is sorted
+            
             if nums[l] <= nums[m]:
-                # If target is within the range of the sorted left half
+               
                 if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            # Else the right half must be sorted
+            
             else:
-                # If target is within the range of the sorted right half
+               
                 if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
