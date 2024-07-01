a1e514bee38f08b41884f6e08a1b888c46ba53d3
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-07-01 13:34:25 -0700

    Time: 39 ms (74.73%) | Memory: 12.2 MB (47.90%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,15 +1,14 @@
 class Solution(object):
     def canVisitAllRooms(self, rooms):
-        visited = set()  # To keep track of visited rooms
-        stack = [0]  # Start with room 0
+        visited = set()  
+        stack = [0]  
         
         while stack:
             room = stack.pop()
             if room not in visited:
                 visited.add(room)
                 for key in rooms[room]:
-                    if key not in visited:
-                        stack.append(key)
+                    stack.append(key)
         
         return len(visited) == len(rooms)
 
