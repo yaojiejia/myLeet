e5286e4b989474cc0d7bb9bc5336826f6ab4dcf5
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-08-03 18:53:16 -0700

    Time: 561 ms (15.52%) | Memory: 25 MB (5.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,12 +4,10 @@ class Solution(object):
         if n == 0:
             return 0
 
-        # Memoization array to store the maximum subarray sum ending at each index
         memo = [0] * n
         memo[0] = nums[0]
 
         for i in range(1, n):
             memo[i] = max(nums[i], memo[i-1] + nums[i])
 
-        # The result is the maximum value in the memoization array
         return max(memo)
