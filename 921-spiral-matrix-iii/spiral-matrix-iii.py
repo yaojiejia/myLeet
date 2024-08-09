c254424a9324c803e04a1c292cfed5ac0902a432
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-08-08 17:03:13 -0700

    Time: 89 ms (26.10%) | Memory: 17.8 MB (74.63%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -1,27 +1,19 @@
-class Solution(object):
-    def spiralMatrixIII(self, rows, cols, rStart, cStart):
-        i,j = rStart, cStart
-
-        diri, dirj = 0, 1 # directions to move
-        twice = 2
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        total = rows * cols
+        directions =[[0, 1], [1,0],[0,-1],[-1,0]]
         res = []
-        moves = 1
-        next_moves = 2
-
-        while len(res) < (rows * cols):
-            if (-1 < i < rows) and ( -1 < j < cols):
-                res.append([i,j])
-            
-            i += diri
-            j += dirj
-            moves -= 1
-            if moves == 0:
-                diri, dirj = dirj, -diri # 90 deg Clockwise
-                twice -= 1
-                if twice == 0:
-                    twice = 2
-                    moves = next_moves
-                    next_moves += 1
-                else:
-                    moves = next_moves - 1
+        r,c = rStart, cStart
+        steps = 1
+        i = 0
+        while len(res) < total:
+            for _ in range(2):
+                dr, dc = directions[i]
+                for _ in range(steps):
+                    if (0 <= r < rows and 0 <= c < cols):
+                        res.append([r,c])
+                    r, c = r + dr, c + dc
+                i = (i + 1) % 4
+            steps += 1
         return res
+        
