b20e8a9a4c33384001db2d814a8c8d0a51bece8e
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2024-08-14 11:31:31 -0700

    Time: 713 ms (49.24%) | Memory: 20.6 MB (68.31%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,33 @@
-class Solution(object):
-    def threeSum(self, nums):
-        ans = []
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+        n = len(nums)
+        res = []
 
-        for i in range(len(nums)):
+        for i in range(n):
             if i > 0 and nums[i] == nums[i-1]:
-                continue
-            else:
-                lp = i + 1
-                rp = len(nums) - 1
-                while lp < rp:
-                    temp = nums[lp] + nums[rp] + nums[i]
-                    if temp < 0:
-                        lp += 1
-                    elif temp > 0:
-                        rp -= 1
-                        
-                    else:
-                        ans.append([nums[i],nums[lp],nums[rp]])
-                        lp += 1
-                        rp -= 1
-                        while nums[lp] == nums[lp - 1] and lp < rp:
-                            lp += 1
-                        while nums[rp] == nums[rp + 1] and lp < rp:
-                            rp -= 1
+                continue  # Skip duplicates at the main index
 
-        
-        return ans
-        
+            lp = i + 1
+            rp = n - 1
+
+            while lp < rp:
+                temp = nums[i] + nums[lp] + nums[rp]
+                if temp == 0:
+                    res.append([nums[i], nums[lp], nums[rp]])
+                    lp += 1
+                    rp -= 1
+                    # Skip duplicates at the left pointer
+                    while lp < rp and nums[lp] == nums[lp - 1]:
+                        lp += 1
+                    # Skip duplicates at the right pointer
+                    while lp < rp and nums[rp] == nums[rp + 1]:
+                        rp -= 1
+                elif temp < 0:
+                    lp += 1
+                else:
+                    rp -= 1
+
+        return res
