4860fbf6fb75a5aa83145ddba468bdecb8a6260d
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-08-18 21:47:36 -0400

    Time: 125 ms (94.67%) | Memory: 32.1 MB (82.31%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -1,23 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def goodNodes(self, root):
-        return self.preOrder(root, root.val)
-    
-    def preOrder(self, root, maxVal):
-        if not root:
-            return 0
-        if root.val >= maxVal:
-            count = 1
-            maxVal = root.val
-        else:
-            count = 0
-        count += self.preOrder(root.left, maxVal)
-        count += self.preOrder(root.right, maxVal)
-        return count
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        blue = 0  
 
+        def good(root, parent_val):
+            nonlocal blue  
+            if not root:
+                return
+            
+            if root.val >= parent_val:
+                blue += 1
+                good(root.left, root.val)
+                good(root.right, root.val)
+            else:
+                good(root.left, parent_val)
+                good(root.right, parent_val)
+            
+            return blue
         
+        return good(root, root.val)
