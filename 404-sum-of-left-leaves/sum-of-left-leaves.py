fe2e0d1eec30553ac31aa1d9663dd461d2b0211c
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-08-18 20:55:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (78.47%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -1,19 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def sumOfLeftLeaves(self, root):
-        l = [0]
-        self.helper(root, l, None)
-        return l[0]
-    def helper(self, root, l, isLeft):
-        if not root:
-            return 
-        if root.left is None and root.right is None and isLeft:
-            l[0] += root.val
-        self.helper(root.left, l, True)
-        self.helper(root.right, l, False)
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         
+        def helper(r):
+            if not r:
+                return 0
+            
+            total = 0
+            # if r.left is a leaf, add its value
+            if r.left and not r.left.left and not r.left.right:
+                total += r.left.val
+            # otherwise, recurse
+            total += helper(r.left)
+            total += helper(r.right)
+            return total
+        
+        return helper(root)
