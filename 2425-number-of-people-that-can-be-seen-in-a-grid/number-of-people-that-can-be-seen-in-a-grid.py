8fcddeb3820bbdbff153ba1a79b7b978f615f781
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-09-14 11:12:58 -0400

    Time: 160 ms (100.00%) | Memory: 29.5 MB (26.83%) - LeetSync

--- 2425-number-of-people-that-can-be-seen-in-a-grid/number-of-people-that-can-be-seen-in-a-grid.py
+++ 2425-number-of-people-that-can-be-seen-in-a-grid/number-of-people-that-can-be-seen-in-a-grid.py
@@ -6,41 +6,33 @@ class Solution:
         res = [[0] * n for _ in range(m)]
         
         # Horizontal pass (right to left)
-        for i in range(m):
+        for r in range(m):
             stack = []
-            for j in range(n - 1, -1, -1):
-                current_height = heights[i][j]
-                
-                # Count all shorter people we can see
-                while stack and heights[i][stack[-1]] < current_height:
+            for c in range(n - 1, -1, -1):
+                ppl = 0
+                while stack and heights[r][c] > stack[-1]:
                     stack.pop()
-                    res[i][j] += 1
-                
-                # Handle equal or taller person
+                    ppl += 1
                 if stack:
-                    if heights[i][stack[-1]] == current_height:
-                        stack.pop()  # Remove equal height person (we see through them)
-                    res[i][j] += 1  # Count one person (equal or taller)
-                
-                stack.append(j)
+                    if heights[r][c] == stack[-1]:
+                        stack.pop()
+                    ppl += 1
+                res[r][c] += ppl
+                stack.append(heights[r][c])
         
         # Vertical pass (bottom to top)
-        for j in range(n):
+        for c in range(n):
             stack = []
-            for i in range(m - 1, -1, -1):
-                current_height = heights[i][j]
-                
-                # Count all shorter people we can see
-                while stack and heights[stack[-1]][j] < current_height:
+            for r in range(m - 1, -1, -1):
+                ppl = 0
+                while stack and heights[r][c] > stack[-1]:
                     stack.pop()
-                    res[i][j] += 1
-                
-                # Handle equal or taller person
+                    ppl += 1
                 if stack:
-                    if heights[stack[-1]][j] == current_height:
-                        stack.pop()  # Remove equal height person (we see through them)
-                    res[i][j] += 1  # Count one person (equal or taller)
-                
-                stack.append(i)
+                    if heights[r][c] == stack[-1]:
+                        stack.pop()
+                    ppl += 1
+                res[r][c] += ppl
+                stack.append(heights[r][c])
         
         return res
