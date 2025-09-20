eefc6fbe35af3920499b7f4b5a9b0ea52b0442ab
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-09-20 18:03:30 -0400

    Time: 186 ms (54.92%) | Memory: 35.3 MB (53.89%) - LeetSync

--- 1043-grid-illumination/grid-illumination.py
+++ 1043-grid-illumination/grid-illumination.py
@@ -3,35 +3,47 @@ from typing import List
 
 class Solution:
     def gridIllumination(self, n: int, lamps: List[List[int]], queries: List[List[int]]) -> List[int]:
-        directions = [(0,0),(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,-1),(-1,1)]
+        directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,-1),(-1,1)]
         row = Counter()
         col = Counter()
         posDia = Counter()
         negDia = Counter()
         lampSet = set()
 
-        for r,c in lamps:
-            if (r,c) in lampSet:
+        # initialize lamp counts
+        for r, c in lamps:
+            if (r, c) in lampSet:
                 continue
-            lampSet.add((r,c))
+            lampSet.add((r, c))
             row[r] += 1
             col[c] += 1
             posDia[r+c] += 1
             negDia[r-c] += 1
 
         res = []
-        for r,c in queries:
+        for r, c in queries:
+            # check illumination
             if row[r] > 0 or col[c] > 0 or posDia[r+c] > 0 or negDia[r-c] > 0:
                 res.append(1)
             else:
                 res.append(0)
 
-            for dx,dy in directions:
-                nr, nc = r+dx, c+dy
-                if (nr,nc) in lampSet:
-                    lampSet.remove((nr,nc))
+            # turn off (r, c) first if lamp exists
+            if (r, c) in lampSet:
+                lampSet.remove((r, c))
+                row[r] -= 1
+                col[c] -= 1
+                posDia[r+c] -= 1
+                negDia[r-c] -= 1
+
+            # then turn off neighbors
+            for dx, dy in directions:
+                nr, nc = r + dx, c + dy
+                if (nr, nc) in lampSet:
+                    lampSet.remove((nr, nc))
                     row[nr] -= 1
                     col[nc] -= 1
                     posDia[nr+nc] -= 1
                     negDia[nr-nc] -= 1
+
         return res
