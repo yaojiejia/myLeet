099ee93058f8fa81c4a7ca8c726d8a7f5839438d
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-09-21 14:32:14 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (8.42%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Base cases
-        if n == 0:
-            return 0
+        # recurrence relation: f(i) = f(i-1) + f(i-2) 
+
         if n == 1:
             return 1
-        
-        dp = [0] * (n + 1)  # Allocate space for dp array
-        dp[0] = 1
+        if n == 2:
+            return 2
+        dp = [0] * (n+1)
         dp[1] = 1
-
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        dp[2] = 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
         
         return dp[n]
