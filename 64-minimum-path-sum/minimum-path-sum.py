1cd40f7ac32e7369520323a4bf225118edfa37b8
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-09-21 16:43:49 -0400

    Time: 18 ms (38.40%) | Memory: 20.1 MB (64.78%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        r = len(grid)
-        c = len(grid[0])
-        new_grid = [[0 for _ in range(c)] for _ in range(r)]
-        new_grid[0][0] = grid[0][0]
-        for i in range(1, c):
-            new_grid[0][i] = new_grid[0][i-1] + grid[0][i] 
+        dp = [[0 for _ in row] for row in grid]
+        dp[0][0] = grid[0][0]
 
-        for i in range(1, r):
-            new_grid[i][0] = new_grid[i-1][0] + grid[i][0]
-        
-        for i in range(1, r):
-            for j in range(1, c):
-                new_grid[i][j] = min(grid[i][j]+new_grid[i-1][j],grid[i][j]+new_grid[i][j-1] )
-        return new_grid[r-1][c-1]
+        rows, cols = len(grid), len(grid[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 and c == 0:
+                    continue
+                top = dp[r-1][c] if r-1 >= 0 else float('inf')
+                left = dp[r][c-1] if c-1 >= 0 else float('inf')
+                dp[r][c] = grid[r][c] + min(top, left)
+
+        return dp[rows-1][cols-1]
