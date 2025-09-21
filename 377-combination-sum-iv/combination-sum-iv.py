7cea87e592a63b113370ded4ece368876826e95b
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-09-21 15:25:51 -0400

    Time: 50 ms (17.54%) | Memory: 17.8 MB (47.35%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        dp = [0] * (target+1)
+        dp[0] = 1
+
+        for i in range(1, target+1):
+            for x in nums:
+                if i - x >= 0:
+                    dp[i] += dp[i-x]
+        print(dp)
+        return dp[target]
