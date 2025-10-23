6aecba8f0babad473e2938cf4e919082447444bd
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-10-22 21:02:47 -0400

    Time: 11 ms (83.37%) | Memory: 20.6 MB (76.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,20 +1,12 @@
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Dictionary to hold sorted string as the key and list of anagrams as values
-        anagrams = {}
-        
+        m = defaultdict(list)
+
         for s in strs:
-            # Sort the string to create a key
-            key = tuple(sorted(s))
-            
-            # If the key doesn't exist, create a new entry in the dictionary
-            if key not in anagrams:
-                anagrams[key] = []
-            
-            # Append the original string to the list of anagrams for this key
-            anagrams[key].append(s)
+            temp = "".join(sorted(s))
+            m[temp].append(s)
+        ans = []
         
-        # Return the list of grouped anagrams
-        return list(anagrams.values())
+        for k, values in m.items():
+            ans.append(values)
+        return ans
