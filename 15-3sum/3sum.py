6ee40ce45da76362d8a4e6c970715a9f7edcd683
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2025-10-22 20:22:10 -0400

    Time: 852 ms (20.28%) | Memory: 20.7 MB (37.05%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,27 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
         res = []
-
+        n = len(nums)
         for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue  # Skip duplicates at the main index
-
-            lp = i + 1
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            lp = i+1
             rp = n - 1
 
             while lp < rp:
-                temp = nums[i] + nums[lp] + nums[rp]
-                if temp == 0:
+                if nums[i] + nums[lp] + nums[rp] == 0:
                     res.append([nums[i], nums[lp], nums[rp]])
-                    lp += 1
                     rp -= 1
-                    # Skip duplicates at the left pointer
-                    while lp < rp and nums[lp] == nums[lp - 1]:
-                        lp += 1
-                    # Skip duplicates at the right pointer
-                    while lp < rp and nums[rp] == nums[rp + 1]:
-                        rp -= 1
-                elif temp < 0:
                     lp += 1
-                else:
-                    rp -= 1
 
+                    while lp < rp and nums[rp+1] == nums[rp]:
+                        rp -= 1
+                    while lp < rp and nums[lp-1] == nums[lp]:
+                        lp += 1
+                elif nums[i] + nums[lp] + nums[rp] > 0:
+                    rp -= 1
+                else:
+                    lp += 1
+                
         return res
